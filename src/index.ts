46b0f2726c2c809c038124de0b4d090950ae792a
fe-xiaoxin <zxin088@gmail.com>
2023-10-07 16:07:38 +0800

    🎉 feat(app): 初始化

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import path from "node:path";
+
+import type { CAC } from "cac";
+import { ACTIVATION, execCommand, loggerInfo } from "code-genius";
+
+const quantity = async (dir: string) => {
+  if (ACTIVATION) {
+    loggerInfo(`clear 参数信息: \n${dir}`);
+  }
+  const root = path.join(process.cwd(), dir);
+  await execCommand("npx", ["cloc", root], {
+    stdio: "inherit",
+  });
+};
+
+const quantityInstaller = () => {
+  return {
+    name: "quantityInstaller",
+    setup: (cli: CAC) => {
+      cli
+        .command("quantity", "运行 cloc 分析并统计代码量")
+        .option("-p, --path <path>", "设置代码路径", {
+          default: ".",
+        })
+        .action(async (options) => {
+          const { path } = options;
+          await quantity(path);
+        });
+    },
+  };
+};
+
+export { quantity, quantityInstaller };
